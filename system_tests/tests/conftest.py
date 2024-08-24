f87282f04db2d159c443b671412e8b77f328fae2
DG266 <daniele.galloppo@protonmail.com>
2024-08-25 01:46:24 +0200

    refactor: rename test data folders and update file paths

--- system_tests/tests/conftest.py
+++ system_tests/tests/conftest.py
@@ -17,7 +17,7 @@ def driver():
 def load_tc_1_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '2.1.1.2.0.2.0.2.0.3.0', 'This file name should not be accepted.xlsx'
+        '..', 'data', 'load_tc_1_(2.1.1.2.0.2.0.2.0.3.0)', 'This file name should not be accepted.xlsx'
     )
 
 
@@ -25,7 +25,7 @@ def load_tc_1_fixture():
 def load_tc_2_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.2.0.0.0.0.0.0.0.0.0', 'stories.txt'
+        '..', 'data', 'load_tc_2_(1.2.0.0.0.0.0.0.0.0.0)', 'stories.txt'
     )
 
 
@@ -33,7 +33,7 @@ def load_tc_2_fixture():
 def load_tc_3_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.1.1.1.0.0.0.0.0.0.0', 'stories.xlsx'
+        '..', 'data', 'load_tc_3_(1.1.1.1.0.0.0.0.0.0.0)', 'stories.xlsx'
     )
 
 
@@ -41,7 +41,7 @@ def load_tc_3_fixture():
 def load_tc_4_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.1.1.3.0.2.0.2.0.3.0', 'stories.xlsx'
+        '..', 'data', 'load_tc_4_(1.1.1.3.0.2.0.2.0.3.0)', 'stories.xlsx'
     )
 
 
@@ -49,7 +49,7 @@ def load_tc_4_fixture():
 def load_tc_5_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.1.2.2.3.2.1.2.0.3.0', 'stories.xlsx'
+        '..', 'data', 'load_tc_5_(1.1.2.2.3.2.1.2.0.3.0)', 'stories.xlsx'
     )
 
 
@@ -57,7 +57,7 @@ def load_tc_5_fixture():
 def load_tc_6_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.1.1.2.0.1.0.0.0.0.0.', 'stories.xlsx'
+        '..', 'data', 'load_tc_6_(1.1.1.2.0.1.0.0.0.0.0)', 'stories.xlsx'
     )
 
 
@@ -65,5 +65,5 @@ def load_tc_6_fixture():
 def load_tc_7_fixture():
     return os.path.join(
         os.path.dirname(__file__),
-        '..', 'data', '1.1.1.2.0.2.0.1.0.0.0.', 'stories.xlsx'
+        '..', 'data', 'load_tc_7_(1.1.1.2.0.2.0.1.0.0.0)', 'stories.xlsx'
     )
