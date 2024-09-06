e15c7759d4565211e27b98c0c6b52d3ede359fcf
DG266 <daniele.galloppo@protonmail.com>
2024-09-06 19:12:17 +0200

    test(refair-server): add unit tests for feature_extraction()
    Implement four unit tests for the feature_extraction() function in REFAIR.py.

--- refair-server/test_REFAIR.py
+++ refair-server/test_REFAIR.py
@@ -1,4 +1,6 @@
-from REFAIR import intersection
+import pandas as pd
+from io import StringIO
+from REFAIR import intersection, feature_extraction
 
 
 class TestIntersection:
@@ -43,3 +45,120 @@ class TestIntersection:
 
         assert result == ["Feature1"]
 
+class TestFeatureExtraction:
+
+    def test_feature_extraction_with_no_tasks(self, mocker):
+        domains_mapping_csv_data = 'Domain,Feature\n' + \
+        'health,age\n'
+        domains_mapping_mock = pd.read_csv(StringIO(domains_mapping_csv_data))
+        tasks_mapping_csv_data = 'Task,Feature\n' + \
+        'clustering,age\n'
+        tasks_mapping_mock = pd.read_csv(StringIO(tasks_mapping_csv_data))
+
+        mocker.patch('REFAIR.domains_mapping', domains_mapping_mock)
+        mocker.patch('REFAIR.tasks_mapping', tasks_mapping_mock)
+
+        domain = 'Health'
+        mltasks = []
+
+        result = feature_extraction(domain, mltasks)
+
+        assert result == {}
+
+    def test_feature_extraction_with_no_matching_features(self, mocker):
+        domains_mapping_csv_data = 'Domain,Feature\n' + \
+        'health,age\n'
+        domains_mapping_mock = pd.read_csv(StringIO(domains_mapping_csv_data))
+        tasks_mapping_csv_data = 'Task,Feature\n' + \
+        'clustering,author\n'
+        tasks_mapping_mock = pd.read_csv(StringIO(tasks_mapping_csv_data))
+
+        mocker.patch('REFAIR.domains_mapping', domains_mapping_mock)
+        mocker.patch('REFAIR.tasks_mapping', tasks_mapping_mock)
+
+        domain = 'Health'
+        mltasks = ['Clustering']
+
+        result = feature_extraction(domain, mltasks)
+
+        assert result == {
+            'Clustering': []
+        }
+
+    def test_feature_extraction_with_single_task(self, mocker):
+        domains_mapping_csv_data = 'Domain,Feature\n' + \
+        'health,age\n' + \
+        'health,ethnicity\n' + \
+        'health,gender\n' + \
+        'information systems,age\n' + \
+        'information systems,brand ownership\n' + \
+        'information systems,gay-friendliness\n' + \
+        'law,age\n' + \
+        'law,ethnicity\n' + \
+        'law,gender\n'
+        domains_mapping_mock = pd.read_csv(StringIO(domains_mapping_csv_data))
+        tasks_mapping_csv_data = 'Task,Feature\n' + \
+        'clustering,age\n' + \
+        'clustering,author\n' + \
+        'clustering,ethnicity\n' + \
+        'data summarization,age\n' + \
+        'data summarization,caste\n' + \
+        'data summarization,gender\n'
+        tasks_mapping_mock = pd.read_csv(StringIO(tasks_mapping_csv_data))
+
+        mocker.patch('REFAIR.domains_mapping', domains_mapping_mock)
+        mocker.patch('REFAIR.tasks_mapping', tasks_mapping_mock)
+
+        domain = 'Health'
+        mltasks = ['Clustering']
+
+        result = feature_extraction(domain, mltasks)
+
+        assert result == {
+            'Clustering': [
+                'age',
+                'ethnicity'
+            ]
+        }
+
+    def test_feature_extraction_with_multiple_tasks(self, mocker):
+        domains_mapping_csv_data = 'Domain,Feature\n' + \
+        'health,age\n' + \
+        'health,ethnicity\n' + \
+        'health,gender\n' + \
+        'information systems,age\n' + \
+        'information systems,brand ownership\n' + \
+        'information systems,gay-friendliness\n' + \
+        'law,age\n' + \
+        'law,ethnicity\n' + \
+        'law,gender\n'
+        domains_mapping_mock = pd.read_csv(StringIO(domains_mapping_csv_data))
+        tasks_mapping_csv_data = 'Task,Feature\n' + \
+        'classification,age\n' + \
+        'classification,geography\n' + \
+        'classification,race\n' + \
+        'clustering,age\n' + \
+        'clustering,author\n' + \
+        'clustering,ethnicity\n' + \
+        'data summarization,age\n' + \
+        'data summarization,caste\n' + \
+        'data summarization,gender\n'
+        tasks_mapping_mock = pd.read_csv(StringIO(tasks_mapping_csv_data))
+
+        mocker.patch('REFAIR.domains_mapping', domains_mapping_mock)
+        mocker.patch('REFAIR.tasks_mapping', tasks_mapping_mock)
+
+        domain = 'Health'
+        mltasks = ['Classification', 'Clustering']
+
+        result = feature_extraction(domain, mltasks)
+
+        assert result == {
+            'Classification' : [
+                'age'
+            ],
+            'Clustering': [
+                'age',
+                'ethnicity'
+            ]
+        }
