074d35b400d5c669926fcb436947ff35924f2b8f
LucoMoro <morelli.2000luca@gmail.com>
2024-09-17 01:25:59 +0200

    style(desktop-app): organize components into frames

--- desktop_app/index.py
+++ desktop_app/index.py
@@ -19,10 +19,10 @@ def open_file():
     if file_path.endswith('.xlsx'):
         # Se è un file .xlsx, crea una label con il nome del file
         file_name = os.path.basename(file_path)
-        label.config(text=f"{file_name}", fg="black")
+        file_name_label.config(text=f"{file_name}", fg="black")
     else:
         # Se non è un file .xlsx, mostra un messaggio di errore
-        label.config(text="Errore: Il file selezionato non è un file .xlsx", fg="red")
+        file_name_label.config(text="Errore: Il file selezionato non è un file .xlsx", fg="red")
         file_path = ""
         user_stories.clear()  # Cancella le user stories salvate
 
@@ -30,7 +30,7 @@ def load_file():
     global user_stories, user_story_labels
     if not file_path:
         # Se non è stato selezionato alcun file valido, mostra un messaggio di errore
-        error_label = Label(root, text="Errore: Nessun file .xlsx selezionato!", fg="red")
+        error_label = Label(main_content, text="Errore: Nessun file .xlsx selezionato!", fg="red")
         error_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
         return
     
@@ -47,7 +47,7 @@ def load_file():
     
     if user_story_col is None:
         # Se la colonna "User Story" non esiste, mostra un messaggio di errore
-        error_label = Label(root, text="Errore: Colonna 'User Story' non trovata!", fg="red")
+        error_label = Label(main_content, text="Errore: Colonna 'User Story' non trovata!", fg="red")
         error_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
         return
 
@@ -64,7 +64,7 @@ def load_file():
     # Stampa ogni riga della colonna "User Story" in una label differente
     row_index = 3
     for cell in user_story_col[1:]:  # Salta l'intestazione
-        user_story_label = Label(root, text=cell.value)
+        user_story_label = Label(main_content, text=cell.value)
         user_story_label.grid(row=row_index, column=0, columnspan=2, padx=10, pady=2, sticky="w")
         row_index += 1
         user_stories.append(cell.value)  # Aggiungi la user story alla lista
@@ -75,39 +75,45 @@ def load_file():
 def save_as_json():
     if not user_stories:
         # Se non ci sono user stories caricate, mostra un messaggio di errore
-        error_label = Label(root, text="Errore: Nessuna User Story da salvare!", fg="red")
+        error_label = Label(main_content, text="Errore: Nessuna User Story da salvare!", fg="red")
         error_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
         return
     
-    # Apri la finestra di dialogo per salvare il file JSON
+    # Allows to save the JSON file with a specific name
     save_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json")])
     
     if save_path:
-        # Scrivi le user stories nel file JSON
+        # Fills the JSON file with US
         with open(save_path, 'w') as json_file:
             json.dump(user_stories, json_file, indent=4)
-        success_label = Label(root, text=f"File salvato: {save_path}", fg="green")
+        success_label = Label(main_content, text=f"File salvato: {save_path}", fg="green")
         success_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
 
-root = Tk()  # Una nuova finestra è definita
+root = Tk()
 root.title("ReFair desktop app")
-root.geometry("600x400")  # Dimensioni della finestra
+root.geometry("600x400")  # window dimensions
 root.iconbitmap('desktop_app/icons/right_arrow_icon.ico')
 
-# Bottone per aprire il file
-button = Button(root, text="Apri File", command=open_file)
+sidebar = Frame(root, background='red', width=200)
+sidebar.pack(fill=Y, side=LEFT)  
+
+main_content = Frame(root, background='yellow')
+main_content.pack(expand=True, fill=BOTH)  
+
+# Select File Button 
+button = ttk.Button(main_content, text="Select file", command=open_file)
 button.grid(row=0, column=0, padx=10, pady=20)
 
-# Bottone per caricare il contenuto del file
-load_button = Button(root, text="Load", command=load_file)
+# Label that shows the name of the file opened
+file_name_label = Label(main_content, text="")
+file_name_label.grid(row=0, column=1, padx=10, pady=20)
+
+# Load Button
+load_button = Button(main_content, text="Load", command=load_file)
 load_button.grid(row=1, column=0, padx=10, pady=10)
 
-# Bottone per salvare il contenuto come JSON
-save_button = Button(root, text="Save as JSON", command=save_as_json)
+# Download all Button
+save_button = Button(main_content, text="Download all", command=save_as_json)
 save_button.grid(row=2, column=0, padx=10, pady=10)
-
-# Label che verrà aggiornata dal bottone "Apri File"
-label = Label(root, text="")
-label.grid(row=0, column=1, padx=10, pady=20)
 
 root.mainloop()  # Necessario per rendere la finestra visibile
