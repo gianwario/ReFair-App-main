980a601c1e6f8f3c988cbc15223bf17819e77a23
LucoMoro <morelli.2000luca@gmail.com>
2024-09-19 10:34:54 +0200

    style(web-app): improve sidebar and titles UI

---
+++ refair-client/src/components/ChapterTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="chapter_title title">
+    <span><h2 class="tag">#</h2></span>
+    <h2 class="chapter chap_one">{{ chapterTitle }}</h2>
+  </div>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: "ChapterTitle",
+  props: {
+    chapterTitle: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
